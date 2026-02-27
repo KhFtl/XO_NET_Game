35144f5d4e2f5ece245dbe4aa36c52f361c994a5
olegangster <72736001+olegangster@users.noreply.github.com>
2026-02-27 20:12:35 +0200

    Fix formatting of winning combinations in CheckWin

--- Server/GameRoom.cs
+++ Server/GameRoom.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -78,14 +78,14 @@ namespace Server
         {
             int[,] wins = new int[,]
             {
-                { 0, 1, 2 }, // Row 1
-                { 3, 4, 5 }, // Row 2
-                { 6, 7, 8 }, // Row 3
-                { 0, 3, 6 }, // Col 1
-                { 1, 4, 7 }, // Col 2
-                { 2, 5, 8 }, // Col 3
-                { 0, 4, 8 }, // Diagonal TL-BR
-                { 2, 4, 6 }  // Diagonal TR-BL
+                { 0, 1, 2 }, 
+                { 3, 4, 5 }, 
+                { 6, 7, 8 }, 
+                { 0, 3, 6 }, 
+                { 1, 4, 7 }, 
+                { 2, 5, 8 }, 
+                { 0, 4, 8 }, 
+                { 2, 4, 6 }  
             };
             int rows = wins.GetLength(0);
             for (int i = 0; i < rows; i++)
