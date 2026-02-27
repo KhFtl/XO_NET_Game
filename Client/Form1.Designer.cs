5ad5f7d7b080c87c5dbd1a6028f468ab496ddd57
olegangster <72736001+olegangster@users.noreply.github.com>
2026-02-27 20:11:19 +0200

    Rename btn_updateRoom to button10 and txt_roomName to textBox1

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Client
+namespace Client
 {
     partial class Form1
     {
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            btn_updateRoom = new Button();
+            button10 = new Button();
             btn_createRoom = new Button();
-            txt_roomName = new TextBox();
+            textBox1 = new TextBox();
             label3 = new Label();
             txt_NickName = new TextBox();
             label4 = new Label();
@@ -59,9 +59,9 @@
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(btn_updateRoom);
+            groupBox1.Controls.Add(button10);
             groupBox1.Controls.Add(btn_createRoom);
-            groupBox1.Controls.Add(txt_roomName);
+            groupBox1.Controls.Add(textBox1);
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(txt_NickName);
             groupBox1.Controls.Add(label4);
@@ -77,15 +77,14 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Керування";
             // 
-            // btn_updateRoom
+            // button10
             // 
-            btn_updateRoom.Location = new Point(598, 165);
-            btn_updateRoom.Name = "btn_updateRoom";
-            btn_updateRoom.Size = new Size(233, 47);
-            btn_updateRoom.TabIndex = 17;
-            btn_updateRoom.Text = "Оновити кімнати";
-            btn_updateRoom.UseVisualStyleBackColor = true;
-            btn_updateRoom.Click += btn_updateRoom_Click;
+            button10.Location = new Point(598, 165);
+            button10.Name = "button10";
+            button10.Size = new Size(233, 47);
+            button10.TabIndex = 17;
+            button10.Text = "Оновити кімнати";
+            button10.UseVisualStyleBackColor = true;
             // 
             // btn_createRoom
             // 
@@ -95,15 +94,14 @@
             btn_createRoom.TabIndex = 16;
             btn_createRoom.Text = "Створити";
             btn_createRoom.UseVisualStyleBackColor = true;
-            btn_createRoom.Click += btn_createRoom_Click;
             // 
-            // txt_roomName
+            // textBox1
             // 
-            txt_roomName.Location = new Point(196, 108);
-            txt_roomName.Name = "txt_roomName";
-            txt_roomName.Size = new Size(396, 39);
-            txt_roomName.TabIndex = 15;
-            txt_roomName.Text = "MyRoom";
+            textBox1.Location = new Point(196, 108);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(396, 39);
+            textBox1.TabIndex = 15;
+            textBox1.Text = "My Room";
             // 
             // label3
             // 
@@ -305,7 +303,6 @@
             lst_Rooms.Name = "lst_Rooms";
             lst_Rooms.Size = new Size(727, 376);
             lst_Rooms.TabIndex = 0;
-            lst_Rooms.DoubleClick += lst_Rooms_DoubleClick;
             // 
             // Form1
             // 
@@ -350,9 +347,9 @@
         private Button button1;
         private GroupBox groupBox3;
         private ListBox lst_Rooms;
-        private Button btn_updateRoom;
+        private Button button10;
         private Button btn_createRoom;
-        private TextBox txt_roomName;
+        private TextBox textBox1;
         private Label label3;
     }
 }
